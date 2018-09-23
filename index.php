9cb8ac746c3f630ddd45e847fbf48b4b0bc01d35
Christian Meier <christianh_meier@hotmail.com>
2018-09-23 13:43:07 +0200

    Adding index.php
    To give response code 200 on /

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Installation is Running</title>
+    </head>
+    <body>
+        <h1>Cloud Instance is Healthy</h1>
+    </body>
+</html>
